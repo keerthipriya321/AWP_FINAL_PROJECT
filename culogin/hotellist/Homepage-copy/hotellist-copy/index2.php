21a5747c5ca54d28334c1d948be071b66affe91f
keerthipriya321 <keerthipriyapinninti04@gmail.com>
2023-05-31 13:02:05 +0530

    initial commit

---
+++ culogin/hotellist/Homepage-copy/hotellist-copy/index2.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hotels Availablity</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link href="favicon.png" rel="icon">
+    <link href="apple-touch-icon.png" rel="apple-touch-icon">
+</head>
+<body>
+	<header>
+		<h1>Hotels Availablity</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="Homepage-copy\index.php">Home</a></li>
+			<li><a href="">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+	<main>
+		<ul class="hotels-list">
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel A</h2>
+					<p>Location: A</p>
+					<p>Price: $100/night</p>
+					<p><a href="Hotel\index.php">Book now</a></p>
+
+				</div>
+			</li>
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel B</h2>
+					<p>Location: B</p>
+					<p>Price: $150/night</p>
+					<p><a href="#">Book now</a></p>
+				</div>
+			</li>
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel C</h2>
+					<p>Location: C</p>
+					<p>Price: $200/night</p>
+					<p><a href="#">Book now</a></p>
+				</div>
+			</li>
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel D</h2>
+					<p>Location: A</p>
+					<p>Price: $100/night</p>
+					<p><a href="#">Book now</a></p>
+				</div>
+			</li>
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel E</h2>
+					<p>Location: A</p>
+					<p>Price: $100/night</p>
+					<p><a href="#">Book now</a></p>
+				</div>
+			</li>
+		</ul>
+	</main>
+</body>
+</html>
---
+++ culogin/hotellist/index2.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hotels Availablity</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link href="favicon.png" rel="icon">
+    <link href="apple-touch-icon.png" rel="apple-touch-icon">
+</head>
+<body>
+	<header>
+		<h1>Hotels Availablity</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="Homepage-copy\index.php">Home</a></li>
+			<li><a href="Homepage-copy\index.php">About</a></li>
+			<li><a href="Homepage-copy\index.php">Contact</a></li>
+		</ul>
+	</nav>
+	<main>
+		<ul class="hotels-list">
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel A</h2>
+					<p>Location: A</p>
+					<p><a href="Hotel-A\index.php">Book now</a></p>
+
+				</div>
+			</li>
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel B</h2>
+					<p>Location: B</p>
+					<p><a href="Hotel-B\index.php">Book now</a></p>
+				</div>
+			</li>
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel C</h2>
+					<p>Location: C</p>
+					<p><a href="Hotel-C\index.php">Book now</a></p>
+				</div>
+			</li>
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel D</h2>
+					<p>Location: A</p>
+					<p><a href="Hotel-D\index.php">Book now</a></p>
+				</div>
+			</li>
+			<li>
+				<div class="hotel-img">
+					<img src="https://via.placeholder.com/150" alt="Hotel Image">
+				</div>
+				<div class="hotel-details">
+					<h2>Hotel E</h2>
+					<p>Location: A</p>
+					<p><a href="Hotel-D\index.php">Book now</a></p>
+				</div>
+			</li>
+		</ul>
+	</main>
+</body>
+</html>
